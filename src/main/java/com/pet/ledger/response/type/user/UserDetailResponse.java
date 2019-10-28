e35108319a063f6ff412b8b5453aa5e6c996b4ee
SON NX <son.nguyen.xuan@ntq-solution.com.vn>
2019-10-28 15:18:00 +0700

    Update code

--- src/main/java/com/pet/ledger/response/type/user/UserDetailResponse.java
+++ src/main/java/com/pet/ledger/response/type/user/UserDetailResponse.java
@@ -1,5 +1,6 @@
 package com.pet.ledger.response.type.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pet.ledger.response.BaseResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,10 +11,19 @@ import lombok.NoArgsConstructor;
 @Data
 public class UserDetailResponse extends BaseResponse {
     private String email;
+
     private String name;
+
+    @JsonProperty("give_name")
     private String giveName;
+
+    @JsonProperty("family_name")
     private String familyName;
+
     private String picture;
+
+    @JsonProperty("phone_number")
     private String phoneNumber;
+
     private String skype;
 }
